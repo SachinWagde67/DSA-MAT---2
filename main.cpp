be119a71b4680ec3c742f223a1b6c831d62088cb
Sachin Wagde <87384264+SachinWagde67@users.noreply.github.com>
2021-11-27 14:15:14 +0000

    Optimization

--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 #include <cstring>
+#include <unistd.h>
 #define CLRSCR "\e[2J\e[H";
 #define RED "\x1b[31m";
 #define BLUE "\x1b[34m";
 #define MAGENTA "\x1b[35m";
 #define WHITE "\x1b[37m";
 #define GREEN "\x1b[32m";
+#define T "\t\t\t\t\t\t\t";
 using namespace std;
 int SIDE; 
-int MINES; 
+int MINES;
+void Intro();
+void Rules();
+void Exit(); 
 
 bool isValid(int row, int col)
 {
@@ -25,7 +30,7 @@ bool isMine(int row, int col, char board[][25])
 
 void getInput(int *x, int *y)
 {
-	cout<<"Enter the coordinates(row, column): ";
+	cout<<"Enter the coordinates (Row(x), Column(y)): ";
 	cin>>*x;
 	cin>>*y;
 	cout<<CLRSCR;
@@ -304,8 +309,106 @@ void getInput()
 	cout<<CLRSCR;
 }
 
-int main()
+void Rules()
 {
-	getInput();
-	playGame();
+	char back;
+	cout<<CLRSCR;
+	cout<<"RULES: "<<endl<<endl;
+	cout<<"1) The board is divided into cells, with mines randomly distributed. "<<endl;
+	cout<<"2) To win, you need to open all the cells. "<<endl;
+	cout<<"3) The number on a cell indicates the number of mines adjacent to it. "<<endl;
+	cout<<"4) Using this you can determine which cells are safe and which contain mines. "<<endl<<endl;
+	cout<<"Press B to go back: ";
+	cin>>back;
+	if(back == 'b' || back == 'B')
+	{
+		Intro();
+	}
+	else
+	{
+		cout<<RED;
+		cout<<"Please press b to go to the main menu! "<<endl<<WHITE;
+		sleep(2);
+		Rules();
+	}
+}
+
+void Exit()
+{
+	char choice;
+	cout<<endl<<RED;
+	cout<<"\t\t\t\t Do you want to exit the game (Y/N): ";
+	cin>>choice;
+	if(choice == 'y' || choice == 'Y')
+	{
+		cout<<RED;
+		cout<<T;cout<<"\tBYE!"<<endl<<WHITE;
+		sleep(2);
+		return;
+	}
+	else if(choice == 'n' || choice == 'N')
+	{
+		Intro();
+	}
+	else
+	{
+		cout<<RED;
+		cout<<"Please press Y for Yes or N for No! "<<endl<<WHITE;
+		sleep(2);
+		Exit();
+	}
+}
+
+void Intro()
+{
+	int choice;
+	while(true)
+	{
+		cout<<CLRSCR;
+		cout<<MAGENTA;
+		cout<<"               _"<<endl;
+		cout<<"   ____ ___   (_)____   ___   _____ _      __ ___   ___   ____   ___   _____"<<endl;
+		cout<<"  / __ `__ \\ / // __ \\ / _ \\ / ___/| | /\\ / // _ \\ / _ \\ / __ \\ / _ \\ / ___/"<<endl;
+		cout<<" / / / / / // // / / //  __/(__  ) | |/ \\/ //  __//  __// /_/ //  __// /"<<endl;
+		cout<<"/_/ /_/ /_//_//_/ /_/ \\___/ \\___/  \\__/\\__/ \\___/ \\___// .___/ \\___//_/"<<endl;
+		cout<<"                                                      /_/"<<endl<<endl;
+		cout<<GREEN;
+		cout<<T;cout<<" 0 0 1 1 1"<<endl;
+		cout<<T;cout<<" 0 0 1 ";cout<<RED;cout<<"*"<<GREEN;cout<<" 1"<<endl;
+		cout<<T;cout<<" 1 1 2 2 2"<<endl<<RED;
+		cout<<T;cout<<" *"<<GREEN;cout<<" 1 1 "<<RED;cout<<"*"<<GREEN;cout<<" 2"<<endl;
+		cout<<T;cout<<" 1 1 1 2 "<<RED;cout<<"*"<<endl<<endl<<WHITE;
+		cout<<T;cout<<"  1) PLAY "<<endl;
+		cout<<T;cout<<"  2) RULES "<<endl;
+		cout<<T;cout<<"  3) EXIT "<<endl<<endl;
+		cout<<"\t\t\t\t\t\t Enter your Input: ";
+		cin>>choice;
+		if(choice == 1)
+		{
+			getInput();
+			playGame();
+			break;
+		}
+		else if(choice == 2)
+		{
+			Rules();
+			break;
+		}
+		else if(choice == 3)
+		{
+			Exit();
+			break;
+		}
+		else
+		{
+			cout<<RED;
+			cout<<"\t\t\t\t\t  Please Enter a valid Choice! "<<WHITE;
+			sleep(2);
+		}
+	}
+}
+
+int main()
+{	
+	Intro();	
 }
